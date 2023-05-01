186159374c9ac6b50e7859d6beecac76f8240155
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2023-05-01 11:27:12 -0700

    shipit

--- MoarSlots/ModCore.cs
+++ MoarSlots/ModCore.cs
@@ -4,14 +4,14 @@ using BepInEx.Configuration;
 using HarmonyLib;
 using ServerSync;
 
-namespace ModFrame
+namespace MoarSlots
 {
     [BepInPlugin(ModGUID, ModName, ModVersion)]
-    public class NewMod : BaseUnityPlugin
+    public class MoarSlotsMod : BaseUnityPlugin
     {
-        private const string ModName = "New Mod";
+        private const string ModName = "MoarSlotsMod";
         private const string ModVersion = "1.0";
-        private const string ModGUID = "some.new.guid";
+        private const string ModGUID = "MoarSlotsMod";
         private static Harmony harmony = null!;
         ConfigSync configSync = new(ModGUID) 
             { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion};
@@ -26,13 +26,20 @@ namespace ModFrame
             return configEntry;
         }
         ConfigEntry<T> config<T>(string group, string name, T value, string description, bool synchronizedSetting = true) => config(group, name, value, new ConfigDescription(description), synchronizedSetting);
+
+        internal static ConfigEntry<int>? Slots;
+
         public void Awake()
         {
+            Slots = config("1 - General", "Number of rows for player to have", 4,
+                new ConfigDescription("Set this to the number of rows you want your inventory to have",
+                    new AcceptableValueRange<int>(4, 16)));
             Assembly assembly = Assembly.GetExecutingAssembly();
             harmony = new(ModGUID);
             harmony.PatchAll(assembly);
             ServerConfigLocked = config("1 - General", "Lock Configuration", true, "If on, the configuration is locked and can be changed by server admins only.");
             configSync.AddLockingConfigEntry(ServerConfigLocked);
+            
         }
     }
 }
---
+++ MoarSlots/Patches.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MoarSlots
+{
+    public class Patches
+    {
+        [HarmonyPatch(typeof(Humanoid), nameof(Humanoid.Awake))]
+        public static class HeightPatch
+        {
+            public static void Prefix(Inventory ___m_inventory)
+            {
+                ___m_inventory.m_height = MoarSlotsMod.Slots!.Value;
+            }
+        }
+
+        [HarmonyPatch(typeof(InventoryGui), nameof(InventoryGui.Awake))]
+        public static class ScrollerPatch
+        {
+            public static void Postfix(InventoryGui __instance)
+            {
+                var t = __instance.transform.Find("root/Player/PlayerGrid");
+                var c = t.Find("Root");
+                var mask = t.gameObject.GetComponent<RectMask2D>();
+                var scroll = t.gameObject.AddComponent<ScrollRect>();
+                scroll.horizontal = false;
+                scroll.scrollSensitivity = 15;
+                scroll.content = c.gameObject.GetComponent<RectTransform>();
+                mask.enabled = true;
+            }
+        }
+    }
+}
